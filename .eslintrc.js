f6ff3f893e8302e23520281c34e75d902ec93810
Kagami <moe.pascal@gmail.com>
2017-04-19 22:25:52 +0800

    fix eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-	'extends': 'airbnb',
-	'plugins': [
-		'react',
-		'jsx-a11y',
-		'import'
-	],
-	'env': {
-		'browser': true,
-		'es6': true,
-		'node': true,
-	},
+  'extends': 'airbnb',
+  'plugins': [
+    'react',
+    'jsx-a11y',
+    'import'
+  ],
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+  },
   "parserOptions": {
     "ecmaFeatures": {
       "jsx": true
     }
   },
   'parser': 'babel-eslint',
-	'rules': {
-		'semi': ['error', 'never'],
-		'import/no-unresolved': [2, { 'ignore': ['views/.*'] }],
+  'rules': {
+    'semi': ['error', 'never'],
+    'import/no-unresolved': [2, { 'ignore': ['views/.*'] }],
     'react/jsx-filename-extension': 'off',
     'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
     'import/extensions': ['error', { 'es': 'never' }],
     'import/no-extraneous-dependencies': 'off',
     'comma-dangle': ['error', 'always-multiline'],
-	},
+    'no-confusing-arrow': ['error', {'allowParens': true}],
+  },
   'settings': {
     'import/resolver': {
       'node': {
