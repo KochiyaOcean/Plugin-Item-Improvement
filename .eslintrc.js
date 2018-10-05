937812bb06c549ae04037dea615d907db9958948
Kagami <moe.pascal@gmail.com>
2018-10-05 16:06:33 +0800

    chore: dep update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,26 @@
 module.exports = {
-  'extends': 'airbnb',
-  'plugins': [
-    'react',
-    'jsx-a11y',
-    'import'
-  ],
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true,
+  extends: ['airbnb', 'poi-plugin'],
+  plugins: ['react', 'jsx-a11y', 'import'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  },
-  'parser': 'babel-eslint',
-  'rules': {
-    'semi': ['error', 'never'],
-    'import/no-unresolved': [2, { 'ignore': ['views/.*'] }],
+  parser: 'babel-eslint',
+  rules: {
+    semi: ['error', 'never'],
+    'import/no-unresolved': [2, { ignore: ['views/.*'] }],
     'react/jsx-filename-extension': 'off',
-    'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
-    'import/extensions': ['error', { 'es': 'never' }],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'import/extensions': ['error', { es: 'never' }],
     'import/no-extraneous-dependencies': 'off',
     'comma-dangle': ['error', 'always-multiline'],
-    'no-confusing-arrow': ['error', {'allowParens': true}],
+    'no-confusing-arrow': ['error', { allowParens: true }],
     'import/prefer-default-export': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'arrow-parens': ['error', 'as-needed'],
-    'no-underscore-dangle': ['error', { 'allow': ['__'] }],
-    'no-console': ['error', { "allow": ["warn", "error"] }],
+    'no-underscore-dangle': ['error', { allow: ['__'] }],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
     'space-in-parens': 'off',
     'object-curly-spacing': 'off',
     'react/jsx-curly-spacing': 'off',
@@ -39,41 +30,13 @@ module.exports = {
     'space-unary-ops': 'off',
     'object-property-newline': 'off',
     'space-infix-ops': 'off',
-    'curly': 'off',
+    curly: 'off',
     'comma-spacing': 'off',
     'array-callback-return': 'off',
     'no-nested-ternary': 'off',
     'no-confusing-arrow': 'off',
     'no-else-return': 'off',
-    'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     'react/forbid-prop-types': 'off'
-  },
-  'settings': {
-    'import/resolver': {
-      'node': {
-        'extensions': ['.js', '.jsx', '.es'],
-        'paths': [__dirname],
-      },
-    },
-    'import/core-modules': [
-      'bluebird',
-      'classnames',
-      'electron',
-      'react',
-      'react-redux',
-      'redux-observers',
-      'reselect',
-      'react-bootstrap',
-      'react-fontawesome',
-      'path-extra',
-      'fs-extra',
-      'lodash',
-      'cson',
-      'react-dom',
-      'redux',
-      'semver',
-      'i18n-2',
-      'prop-types'
-    ],
-  },
+  }
 }
